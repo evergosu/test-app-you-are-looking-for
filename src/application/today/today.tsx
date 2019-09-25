9373f4862658f99a4e642998dc326369fa6163dc
evergosu <evergosu@gmail.com>
2019-09-25 11:54:29 +0300

    Added location info request.

--- src/application/today/today.tsx
+++ src/application/today/today.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+
+import fetchLocationInfo from '../../api/fetch-location-info';
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -15,15 +17,57 @@ const Wrapper = styled.div`
 `;
 
 const Today: React.FC = () => {
+  const [isLoading, hasError, cityName, cityId] = useLocationInfo('moscow');
+
+  if (isLoading) {
+    return <div>This is loader</div>;
+  }
+
+  if (hasError) {
+    return <div>Oops, there is an error</div>;
+  }
+
   return (
     <Wrapper>
       <div>Today</div>
       <div>Icon</div>
       <div>Temperature</div>
       <div>Time</div>
-      <div>Location</div>
+      <div>{`${cityName} (${cityId})`}</div>
     </Wrapper>
   );
 };
+
+type LocationInfo = [boolean, boolean, string?, number?];
+
+function useLocationInfo(location: string): LocationInfo {
+  const [id, setId] = useState<number>();
+
+  const [name, setName] = useState<string>();
+
+  const [hasError, setHasError] = useState(false);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true);
+
+      try {
+        const [{ title, woeid }] = await fetchLocationInfo(location);
+
+        setId(woeid);
+
+        setName(title);
+      } catch (error) {
+        setHasError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, [location]);
+
+  return [isLoading, hasError, name, id];
+}
 
 export default Today;
