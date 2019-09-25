b259477216fbf12eaeb4a9c85bcc4b6fac0a192e
evergosu <evergosu@gmail.com>
2019-09-25 11:59:25 +0300

    Extracted hook.

--- src/application/today/today.tsx
+++ src/application/today/today.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
-import fetchLocationInfo from '../../api/fetch-location-info';
+import useLocationInfo from '../../hooks/use-location-info';
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -17,7 +17,7 @@ const Wrapper = styled.div`
 `;
 
 const Today: React.FC = () => {
-  const [isLoading, hasError, cityName, cityId] = useLocationInfo('moscow');
+  const [isLoading, hasError, cityName = 'Moscow'] = useLocationInfo('moscow');
 
   if (isLoading) {
     return <div>This is loader</div>;
@@ -33,41 +33,9 @@ const Today: React.FC = () => {
       <div>Icon</div>
       <div>Temperature</div>
       <div>Time</div>
-      <div>{`${cityName} (${cityId})`}</div>
+      <div>{cityName}</div>
     </Wrapper>
   );
 };
-
-type LocationInfo = [boolean, boolean, string?, number?];
-
-function useLocationInfo(location: string): LocationInfo {
-  const [id, setId] = useState<number>();
-
-  const [name, setName] = useState<string>();
-
-  const [hasError, setHasError] = useState(false);
-
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    (async () => {
-      setIsLoading(true);
-
-      try {
-        const [{ title, woeid }] = await fetchLocationInfo(location);
-
-        setId(woeid);
-
-        setName(title);
-      } catch (error) {
-        setHasError(true);
-      } finally {
-        setIsLoading(false);
-      }
-    })();
-  }, [location]);
-
-  return [isLoading, hasError, name, id];
-}
 
 export default Today;
---
+++ src/hooks/use-location-info.ts
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+
+import fetchLocationInfo from '../api/fetch-location-info';
+
+type LocationInfo = [boolean, boolean, string?, number?];
+
+export default function useLocationInfo(location: string): LocationInfo {
+  const [id, setId] = useState<number>();
+
+  const [name, setName] = useState<string>();
+
+  const [hasError, setHasError] = useState(false);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true);
+
+      try {
+        const [{ title, woeid }] = await fetchLocationInfo(location);
+
+        setId(woeid);
+
+        setName(title);
+      } catch (error) {
+        setHasError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, [location]);
+
+  return [isLoading, hasError, name, id];
+}
