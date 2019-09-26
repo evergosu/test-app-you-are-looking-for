31da41115068c17c9b000caa4d2bf4ab34cd1b61
evergosu <evergosu@gmail.com>
2019-09-26 16:43:02 +0300

    Added today weather hook.

--- src/application/weather/today/today.tsx
+++ src/application/weather/today/today.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+
+import fetchTodayWeather from '../../../api/fetch-today-weather';
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -20,17 +22,69 @@ type Props = {
 };
 
 const Today: React.FC<Props> = ({ cityName, cityId }) => {
+  const [
+    isLoading,
+    hasError,
+    weatherState,
+    temperature,
+    date,
+  ] = useTodayWeather(cityId);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (hasError) {
+    return <div>Oops, there is an error</div>;
+  }
+
   return (
     <Wrapper>
       <div>Today</div>
-      <div>Icon</div>
-      <div>Temperature</div>
-      <div>Time</div>
+      <div>{weatherState}</div>
+      <div>{`${temperature || '?'}\u00b0C`}</div>
+      <div>{date}</div>
       <div>
         {cityName} {cityId}
       </div>
     </Wrapper>
   );
 };
+
+type LocationWeather = [boolean, boolean, string?, number?, string?];
+
+function useTodayWeather(locationId: number): LocationWeather {
+  const [date, setDate] = useState<string>();
+
+  const [temperature, setTemperature] = useState<number>();
+
+  const [weatherState, setWeatherState] = useState<string>();
+
+  const [hasError, setHasError] = useState(false);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true);
+
+      try {
+        const [w, t, d] = await fetchTodayWeather(locationId);
+
+        setDate(d);
+
+        setTemperature(t);
+
+        setWeatherState(w);
+      } catch (error) {
+        setHasError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, [locationId]);
+
+  return [isLoading, hasError, weatherState, temperature, date];
+}
 
 export default Today;
